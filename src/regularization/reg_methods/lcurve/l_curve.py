66fecc33f5e17a8e3c3ffbfb875ad53a3864eea9
Joshua Kim <kimjosy@Joshuas-MacBook-Pro.local>
2025-09-15 16:50:47 -0700

    Updated imports in reg_methods scripts

--- src/regularization/reg_methods/lcurve/l_curve.py
+++ src/regularization/reg_methods/lcurve/l_curve.py
@@ -32,8 +32,8 @@
 
 # % Set defaults.
 import numpy as np
-from regu.l_corner import l_corner
-from Simulations.l_curve_corner import l_curve_corner
+from regularization.subfunc.l_corner import l_corner
+from regularization.subfunc.l_curve_corner import l_curve_corner
 from numpy.linalg import norm
 
 def l_curve(U,sm,b,method,L,V, nargin, nargout):
